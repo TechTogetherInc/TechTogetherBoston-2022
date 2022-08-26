b633cb3d1116028bb943de2301d26aed7e92d962
Chantel <chantel.theodule@outlook.com>
2022-08-26 13:08:30 -0400

    Add styling and javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+/* Open navbar on click */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("ghost-byte1").style.marginLeft = "250px";
+  }
+  
+  /* Close navbar on click */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("ghost-byte1").style.marginLeft = "0";
+  }
